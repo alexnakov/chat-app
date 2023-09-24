9b8f0b1f0964ce02c0db8e073605a911f166346e
alexnakov <alex.nakov3@gmail.com>
2023-09-24 11:09:39 +0100

    creating register

---
+++ src/pages/Register.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div className='register-container'>
+      <h1>FibreChat</h1>
+      <h2>Register</h2>
+      <form className='register'>
+        <input type="text" placeholder='Name'/>
+        <input type="email"  placeholder='Email'/>
+        <input type="password"  placeholder='Password'/>
+        <label htmlFor='avatar-img'>Avatar</label>
+        <input type="file" name="avatar-img" />
+        <input type='submit' />
+      </form>
+    </div>
+  )
+}
