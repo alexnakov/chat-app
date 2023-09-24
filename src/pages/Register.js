68130e735ac2240831aeb037d983128aca6661ca
alexnakov <alex.nakov3@gmail.com>
2023-09-24 13:56:16 +0100

    avatar done, reg done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AddImgIcon from '../images/add-img-icon.png'
 
 export default function Register() {
   return (
@@ -9,8 +10,12 @@ export default function Register() {
         <input type="text" placeholder='Name'/>
         <input type="email"  placeholder='Email'/>
         <input type="password"  placeholder='Password'/>
-        <label htmlFor='avatar-img'>Avatar</label>
-        <input type="file" name="avatar-img" />
+        <label htmlFor='avatar-img-input' id="avatar-label">
+          <img src={AddImgIcon} alt="Add Image Icon" />
+          <span>Add an avatar</span>
+        </label>
+        <input style={{display: "none"}} type="file" id="avatar-img-input" />
+
         <input type='submit' />
         <div id="have-account">
           <label>Have an account? </label><a href="#">Login</a>
