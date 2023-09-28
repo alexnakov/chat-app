b5516e7118215318dfef7f348bae9a78235a75da
alexnakov <alex.nakov3@gmail.com>
2023-09-28 14:33:47 +0100

    Name and last message well positioned

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -7,6 +7,7 @@ export default function Chats() {
         <img src='https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg' />
         <div className='user-chat-info'>
           <span className='name'>Novak</span>
+          <p className='latest-message'>This is the latest message</p>
         </div>
       </div>
     </div>
