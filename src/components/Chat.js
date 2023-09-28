785dd0ec2e9390ad0514dce1cdad16950e37ba22
alexnakov <alex.nakov3@gmail.com>
2023-09-28 14:44:17 +0100

    chat info name and icons

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { IoCameraSharp, IoPersonAddSharp, IoMenu } from "react-icons/io5";
 
 export default function Chat() {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className='chat-info'>
+        <span className='username'>UserName</span>
+        <div className='chat-icons'>
+          <IoCameraSharp />
+          <IoPersonAddSharp />
+          <IoMenu />
+        </div>
+      </div>
+    </div>
   )
 }
