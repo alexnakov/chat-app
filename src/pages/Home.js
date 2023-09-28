a3a485d2115995fcb71b410dbec208e0f0d16131
alexnakov <alex.nakov3@gmail.com>
2023-09-28 13:31:45 +0100

    migrating to sass

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,11 @@
 import React from 'react'
-import Header from '../components/Header'
-import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+import Chat from '../components/Chat'
 
 export default function Home() {
   return (
-    <div className='home-container'>
-      <div className='grid-parent'>
-        <div className='header-parent'>
-          <Header />
-        </div>
-        <div className='navbar-parent'>
-          <Navbar />
-        </div>
-        <div className='sidebar-parent'>
-          <Sidebar />
-        </div>
-        <div className='chatroom-parent'></div>
+    <div className='home'>
+      <div className='container'>
       </div>
     </div>
   )
