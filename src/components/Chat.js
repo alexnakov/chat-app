58932f2ed7ebf5e0026b547e5bd6a9f914533249
alexnakov <alex.nakov3@gmail.com>
2023-09-28 15:06:29 +0100

    Input bar design proto done

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { IoCameraSharp, IoPersonAddSharp, IoMenu } from "react-icons/io5";
+import Messages from './Messages';
+import Input from './Input';
 
 export default function Chat() {
   return (
@@ -12,6 +14,8 @@ export default function Chat() {
           <IoMenu />
         </div>
       </div>
+      <Messages />
+      <Input />
     </div>
   )
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { IoIosImage, IoIosAttach } from "react-icons/io";
 
 export default function Input() {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type='text' placeholder='Type something ... ' />
+      <div className='send'>
+        <label htmlFor='file'>
+          <IoIosImage className='send-icons' />
+        </label>
+        <input type='file' id='file' style={{display: 'none'}}/>
+        <label htmlFor='file'>      
+          <IoIosAttach className='send-icons' />          
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
