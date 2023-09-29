5ded70251a2301d51afa9dd3ac809ba9875e9790
alexnakov <alex.nakov3@gmail.com>
2023-09-29 19:51:58 +0100

    session: current user becomes null when logout

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
+  const currentUser = useContext(AuthContext)
+
+  console.log(currentUser)
   return (
     <div className="App">
       <BrowserRouter>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { signOut } from 'firebase/auth'
+import { auth } from '../firebase'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <span class='logo'>FibreChat</span>
-      <div class='user'>
+      <span className='logo'>FibreChat</span>
+      <div className='user'>
         <img src='https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg'/>
         <span>User</span>
-        <button>Logout</button>
+        <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
     </div>
   )
