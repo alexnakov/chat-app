f1b84f997144518765266cc95405dd9949c0dd3e
alexnakov <alex.nakov3@gmail.com>
2023-09-29 19:06:42 +0100

    Routing: Set up routes in App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import './App.scss';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
