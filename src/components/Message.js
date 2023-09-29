7c67c79ed98675a8b546e47e8e4c570ec5ae2375
alexnakov <alex.nakov3@gmail.com>
2023-09-29 10:20:16 +0100

    style: Owner and Other msgs w/ imgs styl correctly

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,8 +4,10 @@ export default function Message() {
   return (
     <div className='message'>
       <div className='message-info'>
-        <img src="https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg" />
-        <div>Just now</div>
+        <div class='img-wrapper'>
+          <img src="https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg" />
+        </div>
+        <div class="message-time">Just now</div>
       </div>
       <div className='message-content'>
         <p>Hi mate</p>
