6922a61b98d90544c7f6bb5aff00abc4e8d5ba91
alexnakov <alex.nakov3@gmail.com>
2023-09-29 09:39:45 +0100

    styling message & added scroll to messages comp

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Message() {
   return (
-    <div>Message</div>
+    <div className='message'>
+      <div className='message-info'>
+        <img src="https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg" />
+        <div>Just now</div>
+      </div>
+      <div className='message-content'>
+        <p>Hi mate</p>
+        {/* <img src="https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg" /> */}
+      </div>
+    </div>
   )
 }
