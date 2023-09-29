0996e400c4670efd5ea3ecaf5f6c204ca1e75f63
alexnakov <alex.nakov3@gmail.com>
2023-09-29 18:11:01 +0100

    dependencies: migrated firebase db and api keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "firebase/app"
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getStorage, ref } from "firebase/storage";
+import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRQLiyUN-wPbJgnA7VjncroX_5eAsBvvI",
-  authDomain: "chat-app-ccfde.firebaseapp.com",
-  projectId: "chat-app-ccfde",
-  storageBucket: "chat-app-ccfde.appspot.com",
-  messagingSenderId: "677925212972",
-  appId: "1:677925212972:web:c9d1eda69564ddb27a5289",
-  measurementId: "G-ND49N3B3JN"
+  apiKey: "AIzaSyBPvzERZhakvi9Hc1QKYJNpQ6Yl7FXAlM4",
+  authDomain: "chat-app-f6256.firebaseapp.com",
+  projectId: "chat-app-f6256",
+  storageBucket: "chat-app-f6256.appspot.com",
+  messagingSenderId: "451943981537",
+  appId: "1:451943981537:web:7fa98b7b5c98a69047f2a0"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const storage = getStorage(app)
+export const db = getDatabase(app)
