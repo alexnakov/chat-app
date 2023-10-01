857c25c0226fb0fdec7219f6717f1e24968c461d
alexnakov <alex.nakov3@gmail.com>
2023-10-01 10:45:05 +0100

    Navbar: Authd user displayed with name and profpic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 export default function Navbar() {
+  const {currentUser} = useContext(AuthContext)
+  
   return (
     <div className='navbar'>
       <span className='logo'>FibreChat</span>
       <div className='user'>
-        <img src='https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg'/>
-        <span>User</span>
+        <img src={currentUser.photoURL}/>
+        <span>{currentUser.displayName}</span>
         <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
     </div>
