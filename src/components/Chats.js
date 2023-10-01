426983e64196b8c7fa7f3e8ffd48eedebb2a90b5
alexnakov <alex.nakov3@gmail.com>
2023-10-01 15:04:55 +0100

    UserChats: Chats belonging to user displayed

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,36 +1,39 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore'
+import React, { useContext, useEffect, useState } from 'react'
+import { db } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 export default function Chats() {
+  const [chats, setChats] = useState([])
+
+  const {currentUser} = useContext(AuthContext)
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+      })
+
+      return () => {unsub()}
+    }
+    currentUser.uid && getChats()
+  }, [currentUser.uid])
+
+  console.log(Object.entries(chats))
+
   return (
     <div className='chats'>
-      <div className='user-chat'>
-        <img src='https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg' />
-        <div className='user-chat-info'>
-          <span className='name'>Novak</span>
-          <p className='latest-message'>This is the latest message</p>
-        </div>
-      </div>
-      <div className='user-chat'>
-        <img src='https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg' />
-        <div className='user-chat-info'>
-          <span className='name'>Novak</span>
-          <p className='latest-message'>This is the latest message</p>
-        </div>
-      </div>
-      <div className='user-chat'>
-        <img src='https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg' />
-        <div className='user-chat-info'>
-          <span className='name'>Novak</span>
-          <p className='latest-message'>This is the latest message</p>
-        </div>
-      </div>
-      <div className='user-chat'>
-        <img src='https://cdn.britannica.com/52/152052-050-C6CCD08A/Novak-Djokovic-2011.jpg' />
-        <div className='user-chat-info'>
-          <span className='name'>Novak</span>
-          <p className='latest-message'>This is the latest message</p>
-        </div>
-      </div>
+      {Object.entries(chats).map(chat => {
+        return (
+          <div key={chat[0]} className='user-chat'>
+            <img src={chat[1].userInfo.photoURL} />
+            <div className='user-chat-info'>
+              <span className='name'>{chat[1].userInfo.displayName}</span>
+              <p className='latest-message'>This is the latest message</p>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
