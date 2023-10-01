0cd6acc334edbe1b5a9ddabc06aee4b041f6e7b5
alexnakov <alex.nakov3@gmail.com>
2023-10-01 10:56:04 +0100

    Search: username in inputbox is tracked.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Search() {
+  const [username, setUsername] = useState("")
+
   return (
     <div className='search'>
       <div className='search-form'>
-        <input type='text' placeholder='Find a user...'/>
+        <input type='text' placeholder='Find a user...' onChange={e=>setUsername(e.target.value)} />
       </div>
     </div>
   )
