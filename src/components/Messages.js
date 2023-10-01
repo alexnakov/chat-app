8147666d5c8f898aab31114179f50a117c0f1b04
alexnakov <alex.nakov3@gmail.com>
2023-10-01 16:51:43 +0100

    State: Realtime messages state tracking

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,10 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Message from './Message'
 import { ChatContext } from '../context/ChatContext'
+import { doc, onSnapshot } from 'firebase/firestore'
+import { db } from '../firebase'
 
 export default function Messages() {
   const {data} = useContext(ChatContext)
+  const [messages, setMessages] = useState([])
   
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "cities", "SF"), (doc) => {
+      if (doc.data() === undefined) {
+        setMessages([])
+      } else {
+        setMessages(doc.data().messages)
+      }
+    });
+
+    return () => {
+      unsub()
+    }
+  }, [data.chatId])
 
   return (
     <div className='messages'>
